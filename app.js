8b964fd110b57005060479d18f3f7572667d1136
Antonio Brown <abrownjr12@outlook.com>
2023-07-19 06:18:54 -0500

    cookie stand

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+let Seattle = {
+    mincust :23,
+    maxcust :65, 
+    avgcookiesale:6.3,
 
+
+}
+
+getElementById()
+
+
+let Tokyo = {
+    mincust: 3,
+    maxcust: 24, 
+    avgcookiesale:1.2,
+
+}
+
+getElementById()
+
+let Dubai = {
+    mincust: 11,
+    maxcust: 38, 
+    avgcookiesale: 3.7,
+
+}
+
+getElementById()
+let Paris = {
+    
+    mincust: 20,
+    maxcust: 38, 
+    avgcookiesale: 2.3,
+
+}
+
+getElementById()
+
+
+let Lima  = {
+
+    mincust: 2,
+    maxcust: 16, 
+    avgcookiesale: 4.6,
+}
+
+getElementById()
