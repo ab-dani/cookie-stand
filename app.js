a0539495dcc2a61c023a3e1feae2d3904aeec8ba
Antonio Brown <abrownjr12@outlook.com>
2023-08-04 06:57:22 -0500

    sdkvboidsvbdsoivbdsa

--- app.js
+++ app.js
@@ -104,27 +104,6 @@ Seattle.SEArender()
     }
   };
 
-  const PARIhours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-  const Paris = {
-    locationName: 'Paris',
-    minCustPerHour: 23,
-    maxCustPerHour: 65,
-    avgCookiesPerSale: 6.3,
-    customersEachHour: [],
-    totalDailyCookies: 0, 
-    calcCustomersEachHour: function() {
-      this.customersEachHour.push(random(this.minCustPerHour, this.maxCustPerHour));
-    },
-    calcCookiesEachHour: function() {
-      for (let i = 0; i < PARIhours.length; i++) {
-        this.calcCustomersEachHour();
-        const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
-        this.totalDailyCookies += oneHour;
-      }
-    }
-  };
-
   const LIMAhours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm'];
 
 const Lima = {
@@ -175,3 +154,41 @@ const Lima = {
 
 Lima.render();
 
+
+
+const PARIhours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+  const Paris = {
+    locationName: 'Paris',
+    minCustPerHour: 23,
+    maxCustPerHour: 65,
+    avgCookiesPerSale: 6.3,
+    customersEachHour: [],
+    totalDailyCookies: 0, 
+    calcCustomersEachHour: function() {
+      this.customersEachHour.push(random(this.minCustPerHour, this.maxCustPerHour));
+    },
+    calcCookiesEachHour: function() {
+      for (let i = 0; i < PARIhours.length; i++) {
+        this.calcCustomersEachHour();
+        const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
+        this.totalDailyCookies += oneHour;
+      }
+    },
+
+    render: function (){
+      this.calcCookiesEachHour();
+
+      console.log(this.totalDailyCookies);
+
+      let article = document.getElementById("cityPARI");
+      
+      let cityPARI = document.getElementById("h1");
+      
+      cityPARI.innerText = this.locationName;
+      article.appendChild(cityPARI);
+
+      let cityPARIList = document.createElement("ol")
+    }
+
+  };
