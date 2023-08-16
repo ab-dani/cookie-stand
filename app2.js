a0647063a7853d392f856618dda77bf4fdf39937
Antonio Brown <135006436+ab-dani@users.noreply.github.com>
2023-08-16 01:59:51 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,50 +1,7 @@
 'use strict';
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-/*const SEAhours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-
-  calcCookiesEachHour: function() {
-    for (let i = 0; i < SEAhours.length; i++) {
-      this.calcCustomersEachHour();
-      const oneHour = Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale);
-      this.cookiesPerHour.push(oneHour)
-      this.totalDailyCookies += oneHour;
-    }
-  },
-  SEArender: function (){
-
-    this.calcCookiesEachHour();
-
-    console.log(this.calcCookiesEachHour);
-
-    let article = document.getElementById("citySEA");
-
-    let citySEA = document.createElement("h1");
-
-    citySEA.innerText = this.locationName;
-
-    article.appendChild(citySEA);
-
-    let citySEAList = document.createElement("ol");
-
-    article.appendChild(citySEAList);
-
-    for(let i = 0; i < SEAhours.length;i++){
-      let cookielist = document.createElement("li");
-      cookielist.innerText = `${SEAhours[i]}: ${this.avgCookiesPerSale[i]}`;
-      citySEAList.appendChild(cookielist);
-    }
-const cookielist = document.createElement("li");
-cookielist.textContent = "total: " + 
-this.totalDailyCookies + "cookies";
-citySEAList.appendChild(cookielist);
-
-  }
- 
-};
-Seattle.SEArender()
-*/
 
 
 function CookieConstruct(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerSale) {
