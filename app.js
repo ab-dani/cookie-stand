fd99d3a504afea31241ba4cedbe38fcbf7ef4124
Antonio Brown <abrownjr12@outlook.com>
2023-08-16 17:58:06 -0500

    final push

--- app.js
+++ app.js
@@ -121,3 +121,30 @@ document.getElementById('cookie-stand-table').appendChild(table);
 }
 
 generateTable();
+
+
+document.getElementById('cookieStandForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const location = document.getElementById('location').value;
+  const minCustomers = parseInt(document.getElementById('minCustomers').value); 
+  const maxCustomers = parseInt(document.getElementById('maxCustomers').value);
+  const avgCookies = parseFloat(document.getElementById('avgCookies').value);
+
+  const newCookieStand = new CookieStand(location, minCustomers, maxCustomers, avgCookies);
+
+  const totalDailySales = Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) * avgCookies;
+
+  const tableBody = document.querySelector('#cookieStandTable tbody');
+  const newRow = tableBody.insertRow();
+  newRow.innerHTML = `
+      <td>${newCookieStand.location}</td>
+      <td>${newCookieStand.minCustomers}</td>
+      <td>${newCookieStand.maxCustomers}</td>
+      <td>${newCookieStand.avgCookies}</td>
+      <td>${totalDailySales}</td>
+  `;
+
+  document.getElementById('cookieStandForm').reset();
+});
+
